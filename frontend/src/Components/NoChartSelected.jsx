0f2cdf8ef5183483168ba8dd6f51d02b1cb3774f
Soumyodipto68 <soumyodiptopal77@gmail.com>
2026-05-25 21:37:28 +0530

    a state issue fix

--- frontend/src/Components/NoChartSelected.jsx
+++ frontend/src/Components/NoChartSelected.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { FiMessageSquare } from 'react-icons/fi'
+import { BiMessageEdit } from "react-icons/bi";
 const NoChartSelected = () => {
   return (
     <div className='flex flex-col items-center justify-center h-full'>
-      <FiMessageSquare className='text-6xl p-2' />
+      <BiMessageEdit className='text-6xl p-2' />
       <h1 className='text-2xl font-bold mb-4'>Welcome to Chatter Box</h1>
       <p className='text-black mb-4'>Select a chat to start messaging</p>
     </div>
